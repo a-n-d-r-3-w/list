af2fb99c201c85c42bca43f5d8e346c6a688476a
andrewaliu <liu.anray@gmail.com>
2016-03-24 12:45:00 -0400

    add dependency on ng-focus-if

--- scripts/directives/todos.js
+++ scripts/directives/todos.js
@@ -6,13 +6,5 @@
       templateUrl: 'templates/todos.html',
       controller: 'controller'
     };
-  }).directive('focusOn', function($timeout) {
-    return function(scope, element, attrs) {
-      scope.$on(attr.focusOn, function(e) {
-        $timeout(function() {
-          element[0].focus();
-        });
-      });
-    };
   });
 }());
