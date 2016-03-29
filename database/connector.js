cce04f5b8aec069f30c2086fa758bad1f2aa2570
andrewaliu <liu.anray@gmail.com>
2016-03-29 13:35:43 -0400

    point to mlab database

--- database/connector.js
+++ database/connector.js
@@ -2,8 +2,9 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todo2', function(err) {
+mongoose.connect('mongodb://user2:wC9EJf-*Fn*)v!E^@ds029638.mlab.com:29638/heroku_8s8rk4mw', function(err) {
   if (err) {
+    console.error(err.message);
     console.log('Failed connecting to MongoDB.');
   } else {
     console.log('Successfully connected to MongoDB.');
