a428bb2b3764164489ec65e503f3b5a8ba2a31db
andrewaliu <liu.anray@gmail.com>
2016-03-29 11:28:22 -0400

    add mongoose

---
+++ database/database.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/todo2', function(err) {
+  if (err) {
+    console.log('Failed connecting to MongoDB.');
+  } else {
+    console.log('Successfully connected to MongoDB.');
+  }
+});
--- server.js
+++ server.js
@@ -4,10 +4,8 @@ var express = require('express');
 var server = express();
 var port = 3000;
 
-(function serveStaticFiles() {
-  var DIR_NAME = 'static';
-  server.use('/', express.static(DIR_NAME));
-}());
+var DIR_NAME = 'static';
+server.use('/', express.static(DIR_NAME));
 
 server.listen(port, function() {
   console.log('Server listening on port ' + port + '.');
