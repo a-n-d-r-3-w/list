38499061a28288f36d16f31495cd03d50a605ee1
andrewaliu <liu.anray@gmail.com>
2016-04-07 15:18:31 -0400

    add move item test

--- tests/intern.js
+++ tests/intern.js
@@ -51,7 +51,8 @@ define({
   // Functional test suite(s) to execute against each browser once non-functional tests are completed
   functionalSuites: [
     'tests/functional/addAndDeleteItems',
-    'tests/functional/editItems'
+    'tests/functional/editItems',
+    'tests/functional/moveItems'
   ],
 
   // A regular expression matching URLs to files that should not be included in code coverage analysis
