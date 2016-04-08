3eef8c62bea1b88187da158133b41502f6ac4d7b
andrewaliu <liu.anray@gmail.com>
2016-04-08 16:44:01 -0400

    rename app from todo2 to list

--- tests/functional/moveItems.js
+++ tests/functional/moveItems.js
@@ -2,134 +2,142 @@
 define(function(require) {
   var registerSuite = require('intern!object');
   var assert = require('intern/chai!assert');
+  var ListPage = require('../support/pages/ListPage');
+  var listPage;
 
-  registerSuite({
-    name: 'move items',
-    beforeEach: function() {
-      return this.remote
-        .get(require.toUrl('http://localhost:5000/index.html'))
+  registerSuite(function() {
 
-      .findByCssSelector("[data-tag-test-id='last-add-item-button']")
-        .moveMouseTo().click().end()
+    return {
+      name: 'move items',
+      setup: function() {
+        listPage = new ListPage(this.remote);
+      },
+      beforeEach: function() {
+        return this.remote
+          .get(require.toUrl('http://localhost:5000/index.html'))
 
-      .getActiveElement()
-        .type('Item 1').pressKeys('\uE007').end()
+        .findByCssSelector("[data-tag-test-id='last-add-item-button']")
+          .moveMouseTo().click().end()
 
-      .findByCssSelector("[data-tag-test-id='last-add-item-button']")
-        .moveMouseTo().click().end()
+        .getActiveElement()
+          .type('Item 1').pressKeys('\uE007').end()
 
-      .getActiveElement()
-        .type('Item 2').pressKeys('\uE007').end()
+        .findByCssSelector("[data-tag-test-id='last-add-item-button']")
+          .moveMouseTo().click().end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-0']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 1');
-        }).end().end()
+        .getActiveElement()
+          .type('Item 2').pressKeys('\uE007').end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-1']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 2');
-        }).end().end();
-    },
+        .findByCssSelector("[data-tag-test-id='item-label-0']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 1');
+          }).end().end()
 
-    afterEach: function() {
-      return this.remote
-        .get(require.toUrl('http://localhost:5000/index.html'))
+        .findByCssSelector("[data-tag-test-id='item-label-1']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 2');
+          }).end().end();
+      },
 
-      .findByClassName('item-container').moveMouseTo()
-        .findByClassName('delete-item-button')
-        .click().end().end()
+      afterEach: function() {
+        return this.remote
+          .get(require.toUrl('http://localhost:5000/index.html'))
 
-      .findByClassName('item-container').moveMouseTo()
-        .findByClassName('delete-item-button')
-        .click().end().end()
+        .findByClassName('item-container').moveMouseTo()
+          .findByClassName('delete-item-button')
+          .click().end().end()
 
-      .findAllByClassName('item-container')
-        .then(function(elements) {
-          assert.strictEqual(elements.length, 0);
-        }).end();
-    },
+        .findByClassName('item-container').moveMouseTo()
+          .findByClassName('delete-item-button')
+          .click().end().end()
 
-    'Move to bottom': function() {
-      return this.remote
-        .get(require.toUrl('http://localhost:5000/index.html'))
+        .findAllByClassName('item-container')
+          .then(function(elements) {
+            assert.strictEqual(elements.length, 0);
+          }).end();
+      },
 
-      .findByCssSelector("[data-tag-test-id='item-container-0']")
-        .moveMouseTo()
-        .findByCssSelector("[data-tag-test-id='move-item-button-0']")
-        .click().end().end()
+      'Move to bottom': function() {
+        return this.remote
+          .get(require.toUrl('http://localhost:5000/index.html'))
 
-      .findByCssSelector("[data-tag-test-id='last-move-target']")
-        .click()
-        .end()
+        .findByCssSelector("[data-tag-test-id='item-container-0']")
+          .moveMouseTo()
+          .findByCssSelector("[data-tag-test-id='move-item-button-0']")
+          .click().end().end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-0']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 2');
-        }).end().end()
+        .findByCssSelector("[data-tag-test-id='last-move-target']")
+          .click()
+          .end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-1']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 1');
-        }).end().end()
+        .findByCssSelector("[data-tag-test-id='item-label-0']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 2');
+          }).end().end()
 
-      .refresh()
+        .findByCssSelector("[data-tag-test-id='item-label-1']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 1');
+          }).end().end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-0']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 2');
-        }).end().end()
+        .refresh()
 
-      .findByCssSelector("[data-tag-test-id='item-label-1']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 1');
-        }).end().end();
-    },
+        .findByCssSelector("[data-tag-test-id='item-label-0']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 2');
+          }).end().end()
 
-    'Move to top': function() {
-      return this.remote
-        .get(require.toUrl('http://localhost:5000/index.html'))
+        .findByCssSelector("[data-tag-test-id='item-label-1']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 1');
+          }).end().end();
+      },
 
-      .findByCssSelector("[data-tag-test-id='item-container-1']")
-        .moveMouseTo()
-        .findByCssSelector("[data-tag-test-id='move-item-button-1']")
-        .click().end().end()
+      'Move to top': function() {
+        return this.remote
+          .get(require.toUrl('http://localhost:5000/index.html'))
 
-      .findByCssSelector("[data-tag-test-id='move-target-0']")
-        .click()
-        .end()
+        .findByCssSelector("[data-tag-test-id='item-container-1']")
+          .moveMouseTo()
+          .findByCssSelector("[data-tag-test-id='move-item-button-1']")
+          .click().end().end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-0']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 2');
-        }).end().end()
+        .findByCssSelector("[data-tag-test-id='move-target-0']")
+          .click()
+          .end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-1']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 1');
-        }).end().end()
+        .findByCssSelector("[data-tag-test-id='item-label-0']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 2');
+          }).end().end()
 
-      .refresh()
+        .findByCssSelector("[data-tag-test-id='item-label-1']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 1');
+          }).end().end()
 
-      .findByCssSelector("[data-tag-test-id='item-label-0']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 2');
-        }).end().end()
+        .refresh()
 
-      .findByCssSelector("[data-tag-test-id='item-label-1']")
-        .getVisibleText()
-        .then(function(text) {
-          assert.strictEqual(text, 'Item 1');
-        }).end().end();
-    }
+        .findByCssSelector("[data-tag-test-id='item-label-0']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 2');
+          }).end().end()
+
+        .findByCssSelector("[data-tag-test-id='item-label-1']")
+          .getVisibleText()
+          .then(function(text) {
+            assert.strictEqual(text, 'Item 1');
+          }).end().end();
+      }
+    };
   });
 });
