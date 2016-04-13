c71d63a0402fc653a82c847490c3faaacba9d1fa
andrewaliu <liu.anray@gmail.com>
2016-04-13 09:18:52 -0400

    add shell script to run tests, remove gulp test task

--- gulpfile.js
+++ gulpfile.js
@@ -17,29 +17,6 @@ gulp.task('copyBootstrapFiles', function() {
     .pipe(gulp.dest('static/styles/gulp_generated/bootstrap'));
 });
 
-gulp.task('test', function(done) {
-  var command = [
-    './node_modules/intern/runner.js',
-    'config=tests/intern'
-  ];
-
-  // Spawn the Intern process
-  var child = require('child_process').spawn('node', command, {
-    // Allow Intern to write directly to the gulp process's stdout and
-    // stderr.
-    stdio: 'inherit'
-  });
-
-  // Let gulp know when the child process exits
-  child.on('close', function(code) {
-    if (code) {
-      done(new Error('Intern exited with code ' + code));
-    } else {
-      done();
-    }
-  });
-});
-
 var watcher = gulp.watch(['./**/*.*', '!./static/scripts/bundle.js'], ['default']);
 watcher.on('change', function(event) {
   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
