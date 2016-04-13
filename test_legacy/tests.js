48fb5ea5c93fb731a03ff034b558b58c8f302fbf
andrewaliu <liu.anray@gmail.com>
2016-04-13 09:09:36 -0400

    delete qunit tests

--- test_legacy/tests.js
+++
@@ -1,6 +0,0 @@
-QUnit.test( "hello test", function( assert ) {
-  F('#text-input').text('Hello world!');
-  F('#text-input').visible();
-  assert.ok( 1 == "1", "Passed!" );
-  // F('.sample').text('Hello World!', 'h1 should have text hello world');
-});
