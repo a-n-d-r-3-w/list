ae3e50e44a41a589068c06aac6b97f387284eddc
andrewaliu <liu.anray@gmail.com>
2016-04-13 16:45:52 -0400

    fix mark done and undone

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ server.post('/items', function(req, res) {
     var item = items[i];
     var id = item._id;
     if (id) {
-      queue.push(Item.findByIdAndUpdate(id, {$set: {index: item.index}}));
+      console.info('Name: ' + item.name);
+      console.info('Completed: ' + item.completed);
+      var returnNewItem = true;
+      queue.push(Item.findByIdAndUpdate(id, {$set: {index: item.index, completed: item.completed}}, {new: returnNewItem}));
     } else {
       var newItem = new Item(item);
       queue.push(newItem.save(function (err, newItem) {
